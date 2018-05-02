b1f9ec604f55cd43414d723cc3a49e31c50bf501
Иван <vanoza_007@mail.ru>
2018-05-02 18:28:34 +0600

    Добавил предзагрузку шрифтов, исправил проблемы с наполнением, добавил анимацию для меню и ему же подправил скрипт

--- source/js/open-navigation.js
+++ source/js/open-navigation.js
@@ -2,6 +2,12 @@ var navToggle = document.querySelector(".main-navigation__toggle");
 var navMenu = document.querySelector(".main-navigation__list");
 
 navToggle.classList.add("main-navigation__toggle--js-visible");
+navMenu.classList.add("main-navigation__list--js-hide");
+navMenu.classList.add("main-navigation__list--transition-disabled");
+
+window.onload = function() {
+  navMenu.classList. remove("main-navigation__list--transition-disabled");
+};
 
 navToggle.addEventListener("click", function() {
   if (navMenu.classList.contains("main-navigation__list--js-opened")) {
