4465b91156f69f7e9090d4246551566d13e9b6f9
Иван <vanoza_007@mail.ru>
2018-05-13 18:32:33 +0600

    Добавил минификацию html

--- source/js/open-map.js
+++
@@ -1,7 +0,0 @@
-var iFrameMap = document.querySelector(".contacts__map");
-var hideMap = document.querySelector(".contacts__map-image");
-
-iFrameMap.onload = function () {
-  hideMap.classList.add("contacts__map-image--js-hide");
-  iFrameMap.classList.remove("contacts__map--js-hide");
-};
